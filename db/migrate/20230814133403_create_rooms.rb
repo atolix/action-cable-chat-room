d1667e70b152eb6151d8345f079d56584d173662
atolix <82761106+atolix@users.noreply.github.com>
2023-08-14 22:35:56 +0900

    feat: room table

---
+++ db/migrate/20230814133403_create_rooms.rb
@@ -0,0 +1,11 @@
+class CreateRooms < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rooms do |t|
+      t.string :name, null: false
+      t.string :description
+      t.string :slug, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_14_133237) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_14_133403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_14_133237) do
   create_table "questions", force: :cascade do |t|
     t.string "name", default: "Anonymous"
     t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.string "slug", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
